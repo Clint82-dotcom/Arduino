5819ee5231c7b74864253e0f4e2eeded414b9397
Clint <clint_rankin@hotmail.comgit config --global user.name Clint>
2024-04-22 17:43:27 -0500

    second save

--- Auto_Garden/src/main.cpp
+++ Auto_Garden/src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include "AITRIP_Moisture.h"
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+AITRIP_Moisture sensor1(A0);
+//AITRIP_Moisture sensor2(A1);
+//AITRIP_Moisture sensor3(A2);
+//AITRIP_Moisture sensor4(A3);
+//AITRIP_Moisture sensor5(A4);
+
+void setup()
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  int s1 = sensor1.analogValue();
+  int S1 = sensor1.moisturePercent();
+  
+  Serial.print("Analog Value: ");
+  Serial.println(s1);
+  Serial.print("Moisture Percent: ");
+  Serial.println(S1);
+  delay(1000);
 }
